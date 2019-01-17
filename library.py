e71ff53768eac8a03071c7a49690445d8dff0197
himansu979 <himansusahoo@gmail.com>
2019-01-17 09:51:55 -0600

    added ch03 file

---
+++ library.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+import urllib.request
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy
+
+from sklearn import datasets
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, MinMaxScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.linear_model import LinearRegression, RANSACRegressor
+from sklearn.linear_model import Lasso, Ridge, ElasticNet
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LogisticRegression
+
